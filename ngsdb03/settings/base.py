62945152994220d20cb7129189844752bd511163
Gowthaman Ramasamy <ragowthaman@gmail.com>
2013-10-06 15:35:25 -0700

    added ngsdbview app;created app specific template,static dir

--- ngsdb03/settings/base.py
+++ ngsdb03/settings/base.py
@@ -132,6 +132,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'south',
+    'ngsdbview',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
