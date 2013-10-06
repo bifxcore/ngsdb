b00a642b7047f93029631dc6abc7eba0c596e2fa
Gowthaman Ramasamy <ragowthaman@gmail.com>
2013-10-06 15:05:27 -0700

    SBRIint theme site; admin only;no apps yet;grappelli on;the minimal set;

---
+++ ngsdb03/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^grappelli/', include('grappelli.urls')), # grappelli URLS
+    # Examples:
+    # url(r'^$', 'ngsdb03.views.home', name='home'),
+    # url(r'^ngsdb03/', include('ngsdb03.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
