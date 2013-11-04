194f11aca057dc359ffcf0602fc0c19b4f7f56a5
Gowthaman Ramasamy <ragowthaman@gmail.com>
2013-11-04 14:04:49 -0800

    added ldap authentication system

--- ngsdb03/settings/base.py
+++ ngsdb03/settings/base.py
@@ -1,4 +1,6 @@
 import os
+import ldap
+from django_auth_ldap.config import LDAPSearch, ActiveDirectoryGroupType
 # General Django settings for ngsdb03 project.
 # For host specific settings see corresponding files
 # for example for ngsdb host see ngsdb03.settings.ngsdb
@@ -203,3 +205,70 @@ def get_env_variable(var_name):
    except KeyError:
        error_msg = "Set the %s environment variable" % var_name
        raise ImproperlyConfigured(error_msg)
+
+
+# LDAP authentication
+import logging
+
+logger = logging.getLogger('django_auth_ldap')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    )
+
+# Baseline configuration.
+AUTH_LDAP_SERVER_URI = "ldap://ds.sbri.org"
+
+
+AUTH_LDAP_BIND_DN = "AD Lookup"
+AUTH_LDAP_BIND_PASSWORD = "pass123"
+AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=Users,ou=SBRI,dc=sbri,dc=org",
+    ldap.SCOPE_SUBTREE, "(mailNickname=%(user)s)")
+
+# Set up the basic group parameters.
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=Security Groups,ou=SBRI,dc=sbri,dc=org",
+    ldap.SCOPE_SUBTREE, "(objectClass=group)")
+AUTH_LDAP_GROUP_TYPE = ActiveDirectoryGroupType(name_attr="cn")
+
+# Simple group restrictions
+# AUTH_LDAP_REQUIRE_GROUP = "cn=enabled,ou=django,ou=groups,dc=example,dc=com"
+# AUTH_LDAP_DENY_GROUP = "cn=disabled,ou=django,ou=groups,dc=example,dc=com"
+
+# Populate the Django user from the LDAP directory.
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail"
+}
+
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+    "is_active": "cn=Myler Lab,ou=Security Groups,ou=SBRI,dc=sbri,dc=org",
+    "is_staff": "cn=Myler Lab,ou=Security Groups,ou=SBRI,dc=sbri,dc=org",
+    "is_superuser": "cn=Myler Lab,ou=Security Groups,OU=SBRI,DC=sbri,DC=org",
+
+    #"is_active": "cn=GHBC-Bifx,ou=Security Groups,ou=SBRI,dc=sbri,dc=org",
+    #"is_staff": "cn=GHBC-Bifx,ou=Security Groups,ou=SBRI,dc=sbri,dc=org",
+    #"is_superuser": "cn=GHBC-Bifx,ou=Security Groups,OU=SBRI,DC=sbri,DC=org",
+}
+
+# This is the default, but I like to be explicit.
+AUTH_LDAP_ALWAYS_UPDATE_USER = True
+
+# Use LDAP group membership to calculate group permissions.
+AUTH_LDAP_FIND_GROUP_PERMS = True
+
+# Cache group memberships for an hour to minimize LDAP traffic
+AUTH_LDAP_CACHE_GROUPS = True
+AUTH_LDAP_GROUP_CACHE_TIMEOUT = 3600
+
+
+
+# Keep ModelBackend around for per-user permissions and maybe a local
+# superuser.
+AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    )
