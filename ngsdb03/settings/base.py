4d736871ab9938bd2352bd04cfcb06f589d7a3fc
Gowthaman Ramasamy <gowthaman.ramasamy@seattlebiomed.org>
2014-03-26 11:46:44 -0700

    adjusted css files to be displayed properly in production

--- ngsdb03/settings/base.py
+++ ngsdb03/settings/base.py
@@ -80,7 +80,8 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'static')),
-    "/Users/gramasamy/djcode/ngsdb03/static"
+    "/Users/gramasamy/djcode/ngsdb03/static",
+    "/opt/django-sites/ngsdb/ngsdb03/static"
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
