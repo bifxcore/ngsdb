da72c398ae9869cafe7fb681144a93d386ee732c
Gowthaman Ramasamy <gowthaman.ramasamy@seattlebiomed.org>
2014-03-26 15:32:00 -0700

    changed sample_id to rna_id

--- samples/admin.py
+++ samples/admin.py
@@ -20,16 +20,16 @@ class LibraryAdmin(admin.ModelAdmin):
 
     fieldsets = (
         ('Library', {'fields':('library_code', 'author', 'collaborator', 'bioproject')}),
-        ('Sample Information', {'fields': ('sample_name', 'sample_id','biosample', 'organism', 'lifestage', 'growthphase', 'phenotype', 'genotype', 'source', 'treatment', 'collected_on', 'collected_at', 'collected_by', 'sample_notes', 'is_clonal'), 'classes': ('grp-collapse grp-open',), }),
+        ('Sample Information', {'fields': ('sample_name', 'rna_id','biosample', 'organism', 'lifestage', 'growthphase', 'phenotype', 'genotype', 'source', 'treatment', 'collected_on', 'collected_at', 'collected_by', 'sample_notes', 'is_clonal'), 'classes': ('grp-collapse grp-open',), }),
         ('Library Construction', {'fields': ('librarytype', 'template_material', 'protocol', 'protocol_notes', 'library_creation_date', 'submitted_for_sequencing_on'), 'classes': ('grp-collapse grp-open',), }),
         ('Sequencing Information', {'fields': ('sequence_downloaded_on', 'flowcell_number', 'lane_number', 'index_sequence', 'experiment_notes'), 'classes': ('grp-collapse grp-open',), }),
         ('Analysis Information', {'fields': ('reference_genome', 'reference_genome_version', 'note_for_analysis'), 'classes': ('grp-collapse grp-open',), }),
         ('Data Tracking', {'fields': ('date_created', 'date_modified', 'author_modified'), 'classes': ('grp-collapse grp-open',), })
     )
 
-    list_display = ('library_code', 'librarytype', 'organism', 'lifestage', 'phenotype', 'genotype', 'growthphase', 'treatment', 'template_material', 'reference_genome', 'collaborator', 'bioproject', 'library_creation_date', 'sequence_downloaded_on', 'flowcell_number','lane_number', 'index_sequence')
+    list_display = ('library_code', 'librarytype', 'organism', 'lifestage', 'phenotype', 'genotype', 'growthphase', 'treatment', 'template_material', 'reference_genome', 'collaborator', 'bioproject', 'sample_name', 'rna_id', 'library_creation_date', 'sequence_downloaded_on', 'flowcell_number','lane_number', 'index_sequence')
     list_filter = ['collaborator', 'librarytype__type', 'author__designation', 'organism__organismcode', 'lifestage', 'phenotype', 'genotype', 'growthphase', 'template_material', 'reference_genome', ]
-    search_fields = ['collaborator__firstname', 'collaborator__lastname', 'librarytype__type', 'author__designation', 'author__lastname', 'author__firstname', 'organism__organismcode', 'organism__species', 'organism__genus', 'experiment_notes', 'sample_notes',  'protocol_notes', 'lifestage__lifestage', 'growthphase__growthphase', 'phenotype__phenotype', 'genotype__genotype',  'bioproject__bioproject_code', 'bioproject__bioproject_code', 'biosample__biosample_code', 'protocol__protocol_name', 'library_code', 'flowcell_number', 'index_sequence',]
+    search_fields = ['collaborator__firstname', 'rna_id', 'collaborator__lastname', 'librarytype__type', 'author__designation', 'author__lastname', 'author__firstname', 'organism__organismcode', 'organism__species', 'organism__genus', 'experiment_notes', 'sample_notes',  'protocol_notes', 'lifestage__lifestage', 'growthphase__growthphase', 'phenotype__phenotype', 'genotype__genotype',  'bioproject__bioproject_code', 'bioproject__bioproject_code', 'biosample__biosample_code', 'protocol__protocol_name', 'library_code', 'flowcell_number', 'index_sequence',]
 
 admin.site.register(Library, LibraryAdmin)
 
