d6464e299c4a762b8d2990d85d422ae4e707b29c
Gowthaman Ramasamy <gowthaman.ramasamy@seattlebiomed.org>
2014-06-04 09:37:54 -0700

    minor changes

--- samples/admin.py
+++ samples/admin.py
@@ -6,22 +6,13 @@ from django.contrib import admin
 
 
 class SampleAdmin(admin.ModelAdmin):
+
     list_display = ('sampleid', 'sampletype', 'organism', 'lifestage', 'genotype', 'collaborator', 'treatment', 'sample_quantity', 'bioanalyzer_file_link', 'label_ontube', 'freezer_location', 'sample_notes')
     list_filter = ['sampletype', 'organism', 'lifestage', 'collaborator', 'treatment']
     search_fields = ['sampleid', 'sampletype', 'organism__organismcode', 'lifestage__lifestage', 'growthphase__growthphase', 'phenotype__phenotype', 'genotype__genotype', 'collaborator__firstname', 'collaborator__lastname', 'source', 'collected_by', 'treatment', 'isolation_method', 'sample_quantity', 'biological_replicate_of', 'bioanalyzer_analysis', 'label_ontube', 'freezer_location', 'sample_notes']
 admin.site.register(Sample, SampleAdmin)
 
 class LibraryAdmin(admin.ModelAdmin):
-    #def save_model(self, request, obj, form, change):
-
-        #Over rides default save_model() to insert logged in user from User table (request.user)
-        #get inserted into author_modified
-
-        #if getattr(obj, 'author', None) is None:
-        #    obj.author_modified = request.user
-        #obj.save()
-
-    # format the display in the admin form #for individual gene
     readonly_fields = ("date_modified", "date_created")
 
     fieldsets = (
@@ -34,13 +25,14 @@ class LibraryAdmin(admin.ModelAdmin):
     )
 
     list_display = ('library_code', 'sampleid', 'librarytype', 'organism', 'lifestage', 'phenotype', 'genotype', 'growthphase', 'treatment', 'template_material', 'reference_genome', 'collaborator', 'bioproject', 'library_creation_date', 'sequence_downloaded_on', 'flowcell_number','lane_number', 'index_sequence', 'fastqfile_name')
-    list_filter = ['collaborator', 'librarytype__type', 'author__designation', 'organism__organismcode', 'lifestage', 'phenotype', 'genotype', 'growthphase', 'template_material', 'reference_genome', ]
-    search_fields = ['collaborator__firstname', 'rna_id', 'collaborator__lastname', 'librarytype__type', 'author__designation', 'author__lastname', 'author__firstname', 'organism__organismcode', 'organism__species', 'organism__genus', 'experiment_notes', 'sample_notes',  'protocol_notes', 'lifestage__lifestage', 'growthphase__growthphase', 'phenotype__phenotype', 'genotype__genotype',  'bioproject__bioproject_code', 'bioproject__bioproject_code', 'biosample__biosample_code', 'protocol__protocol_name', 'library_code', 'flowcell_number', 'index_sequence',]
+    list_filter = ['collaborator', 'librarytype__type', 'author__designation', 'organism__organismcode', 'lifestage', 'phenotype', 'genotype', 'growthphase', 'template_material', 'reference_genome' ]
+    search_fields = ['collaborator__firstname', 'rna_id', 'collaborator__lastname', 'librarytype__type', 'author__designation', 'author__lastname', 'author__firstname', 'organism__organismcode', 'organism__species', 'organism__genus', 'experiment_notes', 'sample_notes',  'protocol_notes', 'lifestage__lifestage', 'growthphase__growthphase', 'phenotype__phenotype', 'genotype__genotype',  'bioproject__bioproject_code', 'bioproject__bioproject_code', 'biosample__biosample_code', 'protocol__protocol_name', 'library_code', 'flowcell_number', 'index_sequence']
 
 admin.site.register(Library, LibraryAdmin)
 
 
 class BioprojctAdmin(admin.ModelAdmin):
+
     list_display=('bioproject_code', 'organisms', 'notes')
 
 admin.site.register(Bioproject, BioprojctAdmin)
